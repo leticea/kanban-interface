cf226ddbf5d9b24c1dd1061f639872ae717ed819
leticea <nascimento.leticeah@gmail.com>
2023-04-01 13:12:57 -0300

    feat(index.html):inserting new cards

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const elements = document.querySelectorAll(".card p");
-const LIMIT = 70;
+const todo = document.querySelectorAll(".todo p");
+const done = document.querySelectorAll(".done p");
+const LIMIT = 68;
 
-for (let p of elements) {
+for (let p of todo) {
   const aboveLimit = p.innerText.length > LIMIT;
-  const dotsOrEmpty = aboveLimit ? "..." : "";
+  const dotsOrEmpty = aboveLimit ? "..." : ".";
+  p.innerText = p.innerText.substring(0, LIMIT) + dotsOrEmpty;
+}
+
+for (let p of done) {
+  const aboveLimit = p.innerText.length > LIMIT;
+  const dotsOrEmpty = aboveLimit ? "..." : ".";
   p.innerText = p.innerText.substring(0, LIMIT) + dotsOrEmpty;
 }
